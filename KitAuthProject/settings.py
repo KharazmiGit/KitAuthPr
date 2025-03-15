f996798e5e3600010d60f712d3418a79b14d520b
KharazmiGit <Kharazmi.github@gmail.com>
2025-03-15 08:18:31 +0330

    convet json to selenium is joined to robot processors

--- KitAuthProject/settings.py
+++ KitAuthProject/settings.py
@@ -114,7 +114,7 @@ AUTH_USER_MODEL = 'account.User'
 # REST framework settings
 REST_FRAMEWORK = {
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    #     'rest_framework_simplejwt.auth.JWTAuthentication',
     # ],
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated'
